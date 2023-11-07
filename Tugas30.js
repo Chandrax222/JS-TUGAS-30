5da5b71d520812d9bfbc17329ced1eb1aac62545
Chandra Wiratama Putera <141868663+Chandrax222@users.noreply.github.com>
2023-11-07 23:32:29 +0700

    value.match(/[es]/g)
    value.match(/[es]/g)

---
+++ Tugas30.js
@@ -0,0 +1,7 @@
+function Tugas30(value) {
+    var str2 = value.match(/[es]/g);
+
+    console.log(str2);
+    console.log("Jumlah karakter "+str2.length);
+};
+Tugas30("Para ketua adat mendiskusikan masalah kejahatan yang terjadi di rumah adat");
